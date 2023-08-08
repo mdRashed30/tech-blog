dc37d51a4a26a9d0b17a53aecc34d40872aa0ada
Md Mamatajur Rashed <soinik.us.army@gmail.com>
2023-08-08 00:45:56 -0500

    update routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
-const { Comment } = require("../../models");
+const { Post, User, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
 const withAuth = require("../../utils/auth");
-
 
 
 
